1c49a13bdc22aa33e49473ada3d4e3e363462940
Paul Woods <paul.n.woods@gmail.com>
2023-12-08 14:17:08 -0800

    Added additional tests

--- src/test/java/edu/greenriver/sdev/dice/DiceTests.java
+++ src/test/java/edu/greenriver/sdev/dice/DiceTests.java
@@ -1,17 +1,27 @@
 package edu.greenriver.sdev.dice;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DiceTests {
 
+    private static final int TEST_ROLLS = 50;
+    static Dice d;
+
+    @BeforeAll
+    static void init() {
+        d = new Dice(6, "white");
+    }
+
+
     @Test
     void numbersBetweenOneSix() {
-        Dice d = new Dice(6, "white");
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < TEST_ROLLS; i++) {
             int num = d.roll();
             assertTrue(num > 0 && num < 7);
         }
@@ -19,14 +29,17 @@ public class DiceTests {
 
     @Test
     void sidesEqualsSix() {
-        Dice d = new Dice(6, "white");
-        assertTrue(d.getSides() == 6);
+        assertEquals(6, d.getSides());
     }
 
     @Test
     void colorWhite() {
-        Dice d = new Dice(6, "white");
-        assertTrue(d.getColor().equals("white"));
+        assertEquals("white", d.getColor());
+    }
+
+    @Test
+    void testSides() {
+        assertEquals(6, d.getSides());
     }
 
 
